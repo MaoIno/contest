3648f8f3812d2e393832761e47aaf2614f518634
MaoIno <2848489200@qq.com>
2022-11-01 22:57:07 +0800

    Initial commit

---
+++ Contest/src/Test01.java
@@ -0,0 +1,24 @@
+/**
+ * @avthor MaoIno
+ * @data 2022/10/23
+ */
+public class Test01 {
+    public static void main(String[] args) throws Exception {
+        int[] test = {-2,1,-3,4,-1,2,1,-5,4};
+        Solution solution = new Solution();
+        int s=solution.maxSubArray(test);
+        System.out.println(s);
+
+    }
+}
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
