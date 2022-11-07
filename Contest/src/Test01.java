589d4052d1e388f30bc5c993fb34b6a69c7609f9
MaoIno <2848489200@qq.com>
2022-11-07 21:23:04 +0800

    a

--- Contest/src/Test01.java
+++ Contest/src/Test01.java
@@ -14,6 +14,7 @@ public class Test01 {
 class Solution {
     public int maxSubArray(int[] nums) {
         int pre = 0;
+		int num = 1;
         int maxAns = nums[0];
         for (int x : nums) {
             pre = Math.max(pre + x, x);
